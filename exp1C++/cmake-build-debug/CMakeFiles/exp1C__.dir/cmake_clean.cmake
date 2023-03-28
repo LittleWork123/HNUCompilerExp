caa624f14f7219906d1f703a8e59bff6bd0ffd14
LittleWorl123 <worldpeacedel@gmail.com>
2023-03-28 21:35:20 +0800

    11
    11

---
+++ exp1C++/cmake-build-debug/CMakeFiles/exp1C__.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exp1C__.dir/main.cpp.obj"
+  "CMakeFiles/exp1C__.dir/solve.cpp.obj"
+  "exp1C__.exe"
+  "exp1C__.exe.manifest"
+  "exp1C__.pdb"
+  "libexp1C__.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exp1C__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ exp1C++/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by 86151 on 3/18/2023.
+//
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << "Hello World!";
+}
